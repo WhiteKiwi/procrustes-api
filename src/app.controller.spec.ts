769cc100264b5c2cdd18dfa6a846eab1a2f2bb18
whitekiwi <jh145478@gmail.com>
2020-11-27 13:58:44 +0900

    initial nestjs setting

---
+++ src/app.controller.spec.ts
@@ -0,0 +1,24 @@
+import { Test, TestingModule } from '@nestjs/testing'
+import { AppController } from './app.controller'
+import { AppService } from './app.service'
+
+describe('AppController', () => {
+	let appController: AppController
+	let appService: AppService
+
+	beforeEach(async () => {
+		const app: TestingModule = await Test.createTestingModule({
+			controllers: [AppController],
+			providers: [AppService],
+		}).compile()
+
+		appController = app.get<AppController>(AppController)
+		appService = new AppService()
+	})
+
+	describe('root', () => {
+		it('should return "Hello World!"', () => {
+			expect(appController.getVersion().version).toBe(appService.getVersion())
+		})
+	})
+})
