f78795582fb93c0a0f9bdcd2bf02d1b344e411eb
whitekiwi <jh145478@gmail.com>
2020-12-13 01:17:56 +0900

    test config 구성

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -2,12 +2,24 @@ import { Test, TestingModule } from '@nestjs/testing'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
 
+import { ConfigModule } from '@nestjs/config'
+import configuration from './config/configuration'
+import validationSchema from './config/validation-schema'
+
 describe('AppController', () => {
 	let appController: AppController
 	let appService: AppService
 
 	beforeEach(async () => {
 		const app: TestingModule = await Test.createTestingModule({
+			imports: [
+				ConfigModule.forRoot({
+					isGlobal: true,
+					envFilePath: 'test/.env',
+					load: [configuration],
+					validationSchema,
+				}),
+			],
 			controllers: [AppController],
 			providers: [AppService],
 		}).compile()
--- test/e2e/app.e2e-spec.ts
+++ test/e2e/app.e2e-spec.ts
@@ -1,14 +1,29 @@
 import { Test, TestingModule } from '@nestjs/testing'
 import { INestApplication } from '@nestjs/common'
 import request from 'supertest'
-import { AppModule } from './../src/app.module'
+
+import { AppService } from '../../src/app.service'
+import { AppController } from '../../src/app.controller'
+
+import { ConfigModule } from '@nestjs/config'
+import configuration from '../../src/config/configuration'
+import validationSchema from '../../src/config/validation-schema'
 
 describe('AppController (e2e)', () => {
 	let app: INestApplication
 
 	beforeEach(async () => {
 		const moduleFixture: TestingModule = await Test.createTestingModule({
-			imports: [AppModule],
+			imports: [
+				ConfigModule.forRoot({
+					isGlobal: true,
+					envFilePath: 'test/.env',
+					load: [configuration],
+					validationSchema,
+				}),
+			],
+			controllers: [AppController],
+			providers: [AppService],
 		}).compile()
 
 		app = moduleFixture.createNestApplication()
